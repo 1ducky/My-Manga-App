9218b199a1947f5fb2ddcc8bf286271e758ea3ec
Hadyan Fico Mahardika <realhadyanfico@gmail.com>
2025-06-25 18:49:50 +0700

    del temp folder

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,50 @@
-require('dotenv').config()
+const express = require('express');
+const axios = require('axios');
+const cors= require('cors')
+require('dotenv')
 
-const express= require('express')
-const app = express()
-const Port = process.env.Port
+//fetch
+const Search=require('./Fetch/FetchSeacth')
+const Genre=require('./Fetch/FetchGenre')
+const Newer=require('./Fetch/FetchNewer')
+const Pop=require('./Fetch/FetchPop')
+const Manga=require('./Fetch/FetchManga')
 
-app.listen(Port, () => {
-    console.log(`Server Running at Port:${Port}`)
+
+const PORT = process.env.PORT;
+const app = express();
+
+AllowedOrigins=process.env.AllowedOrigins
+
+
+
+app.use(cors())
+
+//Routing
+app.get('/search/:limit/:key/:offset', async (req,res) =>{
+  const result= await Search(req.params.key,req.params.limit,req.params.offset || 0)
+  res.send(result)
 })
+app.get('/genre/:limit/:uid/:offset', async (req,res) => {
+  const result= await Genre(req.params.uid,req.params.limit,req.params.offset)
+  res.send(result)
+})
+app.get('/new/:limit/:offset', async (req,res) => {
+  const result = await Newer(req.params.limit,req.params.offset)
+  res.send(result)
+})
+app.get('/pop/:limit/:offset', async (req,res) =>{
+  const result = await Pop(req.params.limit,req.params.offset)
+  res.send(result)
+})
+
+app.get('/manga/:id', async (req,res) =>{
+  const result = await Manga(req.params.id)
+  res.send(result)
+})
+
+
+// Starting server
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`✅ Server Running at Port: ${PORT}`);
+});
